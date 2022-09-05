811e81b657db8bd482fabe8da2d78cd3363602f2
DSukhan <denys.sukhan.ki.2020@lpnu.ua>
2022-09-05 20:40:06 +0300

    Created Lab1

---
+++ KI35/Sukhan/Lab1/Sukhan.java
@@ -0,0 +1,10 @@
+package KI35.Sukhan.Lab1;
+
+public class Sukhan {
+    public static void main(String[] args) {
+        String msg = new String();
+        msg = "Sukhan Denys KI-35";
+
+        System.out.println(msg);
+    }
+}
